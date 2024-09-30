ed8ba4ad3a7088e53df17cab38191e632343b753
shashikant021 <shashikantsahu487@gmail.com>
2024-09-30 12:51:57 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,17 @@
 class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-        # Dimensions of the grid
         rows, cols = len(grid), len(grid[0])
         
-        # Function to perform DFS and mark the visited land
         def dfs(r, c):
             if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 'W':
                 return
-            # Mark this land as visited
             grid[r][c] = 'W'
-            # Explore all 4 directions
-            dfs(r - 1, c)  # Up
-            dfs(r + 1, c)  # Down
-            dfs(r, c - 1)  # Left
-            dfs(r, c + 1)  # Right
+        
+            dfs(r - 1, c) 
+            dfs(r + 1, c)  
+            dfs(r, c - 1)  
+            dfs(r, c + 1)  
         
         island_count = 0
         
